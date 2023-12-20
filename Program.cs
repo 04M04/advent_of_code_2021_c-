30560d08fae544232f777a6ea14fc420dcf6ba46
04M04 <micsch04@gmail.com>
2023-12-20 13:25:13 +0100

    add new dotnet cmd project

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+class BingoBoard{
+    private string[,] Board;
+
+    BingoBoard(string[,] board){
+        Board = board;
+    }
+
+    public bool checkIfFinished(){
+        Console.WriteLine("checkIfFinished -> NOT IMPLEMENTED");
+        return false;
+    }
+}
